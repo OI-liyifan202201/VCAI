8df8808e043df2792a6bd7bb0c23773ed745c350
OI-liyifan202201 <liyifan202201@outlook.com>
2024-10-04 21:26:27 +0800

    Update ivc.bat

--- ivc.bat
+++ ivc.bat
@@ -8,3 +8,8 @@ del VSC.exe
 taskkill /f /im code.exe>nul 2>nul
 
 echo IAKIOI > C:\VSc-cpp\install-ok.txt
+
+
+for /f "tokens=2,*" %%A in ('reg query HKCU\Environment /v PATH 2^>nul') do set my_user_path=%%B
+
+setx path "%my_user_path%;%appdata%\..\Local\Programs\Microsoft VS Code\bin"
